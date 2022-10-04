f27f500665e6796154c1469a5d04d9fd84713f73
Kopcsak Patrik <kopcsakpatrk1@gmail.com>
2022-10-04 14:17:25 +0200

    terulet, kerulet

---
+++ src/sokszogOOP/Sokszog.java
@@ -0,0 +1,22 @@
+package sokszogOOP;
+
+public class Sokszog
+{
+    private double a;
+
+    public Sokszog(double a) {
+        this.a = a;
+    }
+
+    public double getA()
+    {
+        return a;
+    }
+
+    public void setA(double a)
+    {
+        this.a = a;
+    }
+
+
+}
---
+++ src/sokszogOOP/Teglalap.java
@@ -0,0 +1,36 @@
+package sokszogOOP;
+
+public class Teglalap extends Sokszog
+{
+    private double b;
+
+
+    public Teglalap(double a, double b) {
+        super(a);
+        this.b = b;
+    }
+
+    public double getB()
+    {
+        return b;
+    }
+
+    public void setB(double b)
+    {
+        this.b = b;
+    }
+
+    public double getKerulet()
+    {
+        return  2 * (this.getA() + this.b);
+    }
+
+    public double getTerulet()
+    {
+        return this.getA() + this.b;
+    }
+
+
+
+
+}
